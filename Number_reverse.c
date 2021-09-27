86aca626e89696a7c1b401d0c2d3da5ec9b5b03e
LearnerForever666 <82526019+LearnerForever666@users.noreply.github.com>
2021-09-27 16:57:19 +0800

    Add files via upload

---
+++ Number_reverse.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<math.h>
+int Number_reverse(int num)
+{
+	int arr[12]={0};
+	int i=0;
+	int j=0;
+	if(num>pow(2,(float)31-1)||num<pow(-2,(float)31))
+	{
+		return 0;
+	}
+	else
+	{
+		i=0;
+		while(num!=0)
+		{
+			arr[i]=num%10;
+			num/=10;
+			i++;
+		};
+		num=0;
+		for(j=0;j<i;j++)
+		{
+			num+=arr[i-j-1]*pow(10,(float)j);
+		};
+		return num;
+	}
+	
+	
+}
+int main()
+{
+	int num=12345;
+	num=Number_reverse(num);
+	printf("%d\n",num);
+	return 0;
+}
