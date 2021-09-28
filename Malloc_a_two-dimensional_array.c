1a8121a6c2de7c3e9bdec6e28918b4c3bf2cea6b
LearnerForever666 <82526019+LearnerForever666@users.noreply.github.com>
2021-09-28 10:53:26 +0800

    Add files via upload

---
+++ Malloc_a_two-dimensional_array.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define MAX 50
+int time=0;
+void charadd(char (**arr)[][MAX],char* padd)
+{
+	if(time==0)
+	{
+		*arr=(char(*)[][MAX])malloc(MAX);
+		time++;
+		strcpy((char*)**arr,padd);
+	}
+	else
+	{
+		*arr=(char(*)[][MAX])realloc(*arr,MAX*(time+1));
+		strcpy(((char*)(**arr))+time*MAX,padd);
+		time++;
+	};
+}
+int main()
+{
+	char (*arr)[][MAX]=NULL;
+	int i=0;
+	int n=0;
+	charadd(&arr,"test");
+	charadd(&arr,"TEST");
+	charadd(&arr,"I win");
+	charadd(&arr,"The game");
+	charadd(&arr,"!!!");
+	for(i=0;i<time;i++)
+	{
+		printf("%s\n",((char*)*arr)+i*MAX);
+	}
+return 0;
+}
